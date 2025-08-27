d3795019db7aa70438f484c13b7d938576edfc7f
kalkidanbehailu55@gmail.com <kalkidanbehailu55@email.com>
2025-08-27 13:21:53 +0300

    feat:change ui bottem bar

--- tip_employee/lib/src/features/home/presentation/widgets/bottom_nav_bar.dart
+++ tip_employee/lib/src/features/home/presentation/widgets/bottom_nav_bar.dart
@@ -12,24 +12,45 @@ class BottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.receipt),
-          label: 'Transactions',
+    return Container(
+      margin: const EdgeInsets.all(12), // space from edges
+      decoration: BoxDecoration(
+        color: Colors.white, // background color of the bar
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 8,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(24),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.receipt),
+              label: 'Transactions',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: currentIndex,
+          selectedItemColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.grey,
+          onTap: onTap,
+          backgroundColor: Colors.transparent, // 👈 remove default rectangle
+          elevation: 0, // 👈 remove shadow from BottomNavigationBar itself
+          type: BottomNavigationBarType.fixed,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: 'settings',
-        ),
-      ],
-      currentIndex: currentIndex,
-      selectedItemColor: Theme.of(context).primaryColor,
-      onTap: onTap,
+      ),
     );
   }
 }
