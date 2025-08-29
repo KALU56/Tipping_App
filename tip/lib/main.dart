f9dda2dc1bad51bcd4893f48663abfc9378165a8
kalkidanbehailu55@gmail.com <kalkidanbehailu55@email.com>
2025-08-29 10:19:13 +0300

    feat:convert theam color

--- tip/lib/main.dart
+++ tip/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:tip/app/themes/app_theme.dart';
 import 'screens/employee_selection/employee_selection_screen.dart';
 
+
 void main() {
-  runApp(CustomerPaymentApp());
+  runApp(const CustomerPaymentApp());
 }
 
 class CustomerPaymentApp extends StatelessWidget {
+  const CustomerPaymentApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Customer Payment',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: AppTheme.lightTheme,   // ✅ Light theme
+      darkTheme: AppTheme.darkTheme, // ✅ Dark theme
+      themeMode: ThemeMode.system,   // ✅ Auto switch based on system
       home: EmployeeSelectionScreen(),
+      debugShowCheckedModeBanner: false, // optional, removes debug banner
     );
   }
 }
