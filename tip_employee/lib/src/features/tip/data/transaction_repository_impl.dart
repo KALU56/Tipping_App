28a31fe57206e4d1ad1be1ce036d69c4920d6595
kalkidanbehailu55@gmail.com <kalkidanbehailu55@email.com>
2025-09-19 23:47:13 +0300

    done the repository

---
+++ tip_employee/lib/src/features/tip/data/transaction_repository_impl.dart
@@ -0,0 +1,33 @@
+import 'package:tip_employee/src/core/service/http_service/http_service.dart';
+import 'package:tip_employee/src/core/service/http_service/http_service_response_model.dart';
+import 'package:tip_employee/src/features/tip/data/models/transaction_model.dart';
+import 'package:tip_employee/src/features/tip/domain/transaction_repository.dart';
+
+class TransactionRepositoryImpl implements TransactionRepository {
+  final HttpService _httpService;
+
+  TransactionRepositoryImpl(this._httpService);
+
+  @override
+  Future<List<TransactionModel>> getTransactions() async {
+    final HttpServiceResponseModel response =
+        await _httpService.get('/employees/transactions');
+
+    if (response.staticCode == 200) {
+      final data = response.data;
+
+      if (data is Map && data['transactions'] is List) {
+        return (data['transactions'] as List)
+            .map((json) => TransactionModel.fromJson(json))
+            .toList();
+      }
+
+      if (data is List) {
+        return data.map((json) => TransactionModel.fromJson(json)).toList();
+      }
+    }
+
+    // Error or empty response
+    return [];
+  }
+}
---
+++ tip_employee/lib/src/features/tip/domain/transaction_repository.dart
@@ -0,0 +1,6 @@
+import 'package:tip_employee/src/features/tip/data/models/transaction_model.dart';
+
+
+abstract class TransactionRepository {
+  Future<List<TransactionModel>> getTransactions();
+}
